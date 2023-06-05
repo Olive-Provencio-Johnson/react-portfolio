3952ce1043f3686a613b5f0b88d4e02a7855b654
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-06-05 02:08:51 -0700

    added github profile to the contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,8 +9,10 @@ const Contact = () => {
       </div>
       <div className="contact-info">
       <p>For enquiries, please email me at: <a href="mailto:OliveProWebDev@gmail.com">OliveProWebDev@gmail.com</a></p>
+      <br></br>
+        <a href="https://github.com/Olive-Provencio-Johnson" target="_blank" rel="noopener noreferrer">GitHub Profile</a>
         <br></br>
-        <a href="https://www.linkedin.com/in/olivia-provencio-johnson/" target="_blank" rel="noopener noreferrer">My LinkedIn Profile</a>
+        <a href="https://www.linkedin.com/in/olivia-provencio-johnson/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a>
       </div>
     </div>
   );
